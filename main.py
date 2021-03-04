a7a3cf2b8f8be42839d570c680693302d1f99f1d
Lars Kue <lars@kuehmichel.de>
2021-03-04 18:55:09 +0100

    Random Forest, train test split, colours etc.

--- main.py
+++ main.py
@@ -1,23 +1,22 @@
-
 from sklearn.datasets import load_breast_cancer
 from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
 from sklearn.decomposition import PCA
+from sklearn.ensemble import RandomForestClassifier
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 
 
-def main(argv: list) -> int:
+def to_colours(x):
+    return list(map(lambda l: "C1" if l else "C0", ~np.isclose(x, 0)))
 
+
+def main(argv: list) -> int:
     breast = load_breast_cancer()
 
     features = breast.data
     labels = breast.target
-
-    # normalize features
-    features = StandardScaler().fit_transform(features)
-
-    print(np.mean(features), np.std(features))
 
     features_labels = np.concatenate((features, labels[:, np.newaxis]), axis=1)
     column_names = np.append(breast.feature_names, "label")
@@ -27,40 +26,77 @@ def main(argv: list) -> int:
 
     print(df.head())
 
-    pca = PCA(n_components=16)
-    pcomponents = pca.fit_transform(features, labels)
+    train_features, test_features, train_labels, test_labels = train_test_split(features, labels, random_state=0)
 
-    print(pcomponents.shape)
+    # normalize features
+    scaler = StandardScaler()
+    train_features = scaler.fit_transform(train_features)
+    test_features = scaler.transform(test_features)
+
+    print(np.mean(train_features), np.std(train_features))
+    print(np.mean(test_features), np.std(test_features))
+
+    pca = PCA()
+    train_components = pca.fit_transform(train_features)
+
+    print(train_components.shape)
 
     cs = np.cumsum(pca.explained_variance_ratio_)
 
-    # for plotting
-    nc = 2
-
     # find the number of components for 99% confidence
-    # confidence = 0.99
-    # nc = np.argmax(cs > confidence)
-    # print(nc)
+    confidence = 0.99
+    nc = np.argmax(cs > confidence)
+    print(nc)
 
     # redo the pca with less components
     pca = PCA(n_components=nc)
-    pcomponents = pca.fit_transform(features, labels)
+    train_components = pca.fit_transform(train_features)
+    test_components = pca.transform(test_features)
 
-    print(pcomponents.shape)
+    print(train_components.shape)
 
-    if nc == 2:
-        plt.figure(figsize=(10, 9))
-        plt.scatter(pcomponents[:, 0], pcomponents[:, 1], marker=".", c=labels)
-        plt.xlabel("PC 1")
-        plt.ylabel("PC 2")
-        plt.savefig("plot.pdf")
-        plt.show()
+    train_colours = to_colours(train_labels)
+    test_colours = to_colours(test_labels)
 
+    fig, axes = plt.subplots(1, 2, figsize=(20, 9))
+    fig.suptitle("Comparison of Feature Separation in Most Important vs. Least Important Principal Components")
+    axes[0].scatter(train_components[:, 0], train_components[:, 1], marker=".", c=train_colours)
+    axes[0].scatter(test_components[:, 0], test_components[:, 1], marker=".", c=test_colours)
+    axes[0].set_xlabel("PC 0")
+    axes[0].set_ylabel("PC 1")
+    axes[0].set_title("Most Important")
+    axes[0].legend(["Benign", "Malignant"])
+    axes[1].scatter(train_components[:, -2], train_components[:, -1], marker=".", c=train_colours)
+    axes[1].scatter(test_components[:, 0], test_components[:, 1], marker=".", c=test_colours)
+    axes[1].set_xlabel(f"PC {nc - 2}")
+    axes[1].set_ylabel(f"PC {nc - 1}")
+    axes[1].set_title("Least Important")
+    axes[1].legend(["Benign", "Malignant"])
+
+    plt.savefig("plot.pdf")
+
+    clf = RandomForestClassifier(max_depth=5, random_state=0)
+    clf.fit(train_components, train_labels)
+
+    # test the classifier
+    predictions = clf.predict(test_components)
+
+    incorrect = predictions != test_labels
+    error_rate = np.sum(incorrect) / len(predictions)
+
+    # < 10%
+    print(f"Error Rate: {100 * error_rate:.2f}%")
+
+    # show the confusion matrix
+    conf_mat = pd.crosstab(test_labels, predictions, normalize=True, rownames=["Actual"], colnames=["Predicted"])
+    print(conf_mat)
+
+    plt.show()
 
     return 0
 
 
-
 if __name__ == "__main__":
     import sys
+
     main(sys.argv)
